7949fd46dd410787c21c9bc40a34a0aea1732543
gowtham758550 <gowtham758550@gmail.com>
2022-02-09 17:53:12 +0530

    demo button added

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,7 @@
 import { Card,
          Image 
         } from "react-bootstrap"
+import Buttons from  './Buttons'
 import '../styles/Landing.css'
 
 export default function Landing () {
@@ -18,7 +19,21 @@ export default function Landing () {
                         <span className="text-white"> Bootstrap framework</span>
                         .</p> 
                     </div>
-                    <Image fluid src="https://bootstrapstudio.io/assets/img/app_4.jpg" />
+                    <div className="py-5">
+                        <Image fluid src="https://bootstrapstudio.io/assets/img/app_4.jpg" />
+                    </div>
+                    <div>
+                        <Buttons 
+                            value={
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                                        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                                    </svg>
+                                    <strong>Run Broswer Demo</strong>
+                                </span>
+                            }
+                            variant="primary" />
+                    </div>
                 </Card.Text>
             </Card.Body>
         </Card>
